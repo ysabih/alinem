2cc214cd7dd89fa236e997a733a7b7e7bae07119
Yussuf <sabihyussuf@gmail.com>
2021-07-27 11:17:10 +0100

    [Server] Add integration tests skeleton
    They're not wokring for now

---
+++ Alinem.IntegrationTests/GameHubTests.cs
@@ -0,0 +1,39 @@
+﻿using Alinem.Models;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Alinem.IntegrationTests
+{
+	[TestFixture]
+	public class GameHubTests
+	{
+		[Test]
+		public async Task TestInitGame()
+		{
+//			string textPayload = @"{
+//		""requesterPlayerName"": ""IntegrationTests"",
+//        ""gameType"": ""VS_COMPUTER"",
+//        ""requesterTurn"": ""TWO""
+//}";
+
+			var payload = new InitGameRequest
+			{
+				GameType = GameType.VS_COMPUTER,
+				RequesterPlayerName = "IwillGrillYourMother",
+				RequesterTurn = PlayerTurn.ONE
+			};
+
+			var connection = new HubConnectionBuilder()
+			.WithUrl("http://localhost:5000/gamehub")
+			.AddNewtonsoftJsonProtocol()
+			.Build();
+			await connection.StartAsync().ConfigureAwait(false);
+
+			var response = await connection.InvokeAsync<GameBoardState>("InitGame", payload).ConfigureAwait(false);
+
+		}
+	}
+}
